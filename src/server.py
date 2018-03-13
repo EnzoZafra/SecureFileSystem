5c2c56fab51cb387c6d7b0f9f837627fc6e87668
enzozafra <zafra@ualberta.ca>
2018-03-13 16:01:11 -0600

    apply gitignore

--- src/server.py
+++ src/server.py
@@ -13,6 +13,44 @@ while True:
         print( "connected from", address )
     else:
         print(" Response from Client")
-        inputToClient = "Command Recieved was: " + client.recv(MAX_BYTE).decode()
+        cmd = client.recv(MAX_BYTE).decode()
+        inputToClient = "Command Recieved was: " + cmd
+        parseCommand(cmd)
         byteinputToClient = inputToClient.encode()
         client.send(byteinputToClient)
+
+def parseCommand(cmd):
+  splitCmd = cmd.split("|")
+  cmd = cmd[0]
+
+  if cmd == "ls":
+    server_ls()
+  elif cmd == "cd":
+    server_cd(cmd[1])
+  elif cmd == "mv" or cmd == "move":
+    server_mv(cmd[1])
+  elif cmd == "cat":
+    server_cat(cmd[1])
+  elif cmd == "logout":
+    server_logout()
+  elif cmd == "open" or cmd == "vim" or cmd == "edit":
+    server_edit(cmd[1])
+
+def server_ls():
+  print("To be implemented")
+
+def server_cd(directory):
+  print("To be implemented")
+
+def server_mv(destination):
+  print("To be implemented")
+
+def server_cat(filename):
+  print("To be implemented")
+
+def server_logout():
+  print("To be implemented")
+
+def server_open(filename):
+  print("To be implemented")
+
