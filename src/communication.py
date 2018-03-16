e6d85ccb77593f1f438beb2957121f51178ef7b0
enzozafra <zafra@ualberta.ca>
2018-03-16 09:24:28 -0600

    use cPickle to encode messages

--- src/communication.py
+++ src/communication.py
@@ -5,32 +5,32 @@ import struct
 marshall = cPickle.dumps
 unmarshall = cPickle.loads
 
-# def send(channel, *args):
-#     buf = marshall(args)
-#     value = socket.htonl(len(buf))
-#     size = struct.pack("L",value)
-#     channel.send(size)
-#     channel.send(buf)
+def send(channel, *args):
+    buf = marshall(args)
+    value = socket.htonl(len(buf))
+    size = struct.pack("L",value)
+    channel.send(size)
+    channel.send(buf)
 
-# def receive(channel):
+def receive(channel):
 
-#     size = struct.calcsize("L")
-#     size = channel.recv(size)
-#     try:
-#         size = socket.ntohl(struct.unpack("L", size)[0])
-#     except struct.error, e:
-#         return ''
+    size = struct.calcsize("L")
+    size = channel.recv(size)
+    try:
+        size = socket.ntohl(struct.unpack("L", size)[0])
+    except struct.error, e:
+        return ''
 
-#     buf = ""
+    buf = ""
 
-#     while len(buf) < size:
-#         buf = channel.recv(size - len(buf))
+    while len(buf) < size:
+        buf = channel.recv(size - len(buf))
 
-#     return unmarshall(buf)[0]
+    return unmarshall(buf)[0]
 
-def send(socket, msg):
-  socket.send(msg.encode())
+# def send(socket, msg):
+#   socket.send(msg.encode())
 
-def receive(socket):
-  msg = socket.recv(1024).decode()
-  return msg
+# def receive(socket):
+#   msg = socket.recv(1024).decode()
+#   return msg
